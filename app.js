b42e5ab6853ee0de6f95ea5e91ef883fc632b19f
Océane <docasaloceane05@gmail.com>
2026-03-19 17:10:46 +0100

    Init projet

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connexion à MongoDB réussie'))
+.catch((err) => console.error('Erreur de connexion :', err));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.set('view engine' , 'ejs');
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
---
+++ models/Catway.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const catwaySchema = new Schema({
+    catwayNumber: {type: Number, required: true, unique: true},
+    catwayType: {type: String, enum: ['long', 'short'], required: true},
+    catwayState: {type: String, required: true}
+});
+
+module.exports = mongoose.model('Catway', catwaySchema);
