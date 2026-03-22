01776d92b325ed7b904a3c08690a1142a3503c78
Océane <docasaloceane05@gmail.com>
2026-03-22 12:00:01 +0100

    Mise en place

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const catwayRoute = require('./routes/catway');
-const reservationRoute = require('./routes/reservation');
+const path = require('path');
 require('dotenv').config();
 
+const catwayRoute = require('./routes/catway');
+const reservationRoute = require('./routes/reservation');
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connexion à MongoDB réussie'))
 .catch((err) => console.error('Erreur de connexion :', err));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
 app.use('/api/catways', catwayRoute);
 app.use('/api/catways', reservationRoute);
-
-app.set('view engine' , 'ejs');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur lancé sur le port ${PORT}`));
