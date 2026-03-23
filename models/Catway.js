5566246732dd4686f5502578d1937a40bcf42f5e
Océane <docasaloceane05@gmail.com>
2026-03-23 17:35:00 +0100

    Ajout des 24 catways

--- models/Catway.js
+++ models/Catway.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const catwaySchema = new Schema({
-    catwayNumber: {type: Number, required: true, unique: true},
+    catwayNumber: {type: Number, required: true, unique: true, min:[0]},
     catwayType: {type: String, enum: ['long', 'short'], required: true},
     catwayState: {type: String, required: true}
 });
