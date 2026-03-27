48adaf45ad3b978e33341e7ff2926c251aa47d3e
Océane <docasaloceane05@gmail.com>
2026-03-27 17:07:47 +0100

    Ajout user dans seed

--- seed.js
+++ seed.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 const Catway = require('./models/Catway');
 const Reservation = require('./models/Reservation');
-const fs = require('fs');
-require('dotenv').config();
+const User = require('./models/User');
 
 async function seedDatabase() {
     try{
@@ -13,15 +15,29 @@ async function seedDatabase() {
         console.log("Anciennes données supprimées.");
         await Catway.insertMany(data);
         console.log("Succès : les 24 catways ont été bien importés.");
+
         if (fs.existsSync('./reservations.json')) {
             const resData = JSON.parse(fs.readFileSync('./reservations.json', 'utf-8'));
             await Reservation.deleteMany({}); 
             await Reservation.insertMany(resData);
             console.log("Succès : les réservations ont été importées.");
         }
-        process.exit();
+
+        await User.deleteMany({});
+        const passwordToHash = process.env.ADMIN_PASSWORD || "DefaultPass123";
+        const hashedPassword = await bcrypt.hash(passwordToHash, 10);
+        const admin = new User({
+            username: "Marie",
+            email: "marie05@gmail.com",
+            password: hashedPassword
+        });
+        await admin.save();
+        console.log("Succès : L'utilisateur Marie a été créé avec le MDP du .env");
+        console.log("\nBase de données prête pour le test");
+        process.exit(0);
+
     } catch (error){
-        console.error("Erreur d'import :", error);
+        console.error("Erreur seed :", error);
         process.exit(1);
     }
 }
